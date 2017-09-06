3b48ad0c55d7be8dd1efd5b325b048cb66f4f4eb
murog <guillermina.muro@gmail.com>
2017-09-06 10:33:13 -0700

    added insufficient funds error

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,4 @@
+require_relative 'insufficient_funds_error'
 module Bank
   class Account
     attr_reader :id, :balance
@@ -16,8 +17,7 @@ module Bank
       end
 
       if @balance - amount < 0
-        puts "Cannot withdraw; balance would go negative"
-        return @balance
+        raise InsufficientFundsError.new("Cannot withdraw; balance would go negative.\nCurrent Balance: #{@balance}")
       end
 
       @balance -= amount
